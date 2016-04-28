0e5f5805661dd4ad97e47bb8f14627d54ac08ed6
YehYungCheng <yehyungcheng@yungchengtekimacbook-air.local>
2016-04-28 20:43:26 +0800

    Initial Commit

---
+++ ezSocial/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ezSocial
+//
+//  Created by YehYungCheng on 2016/4/28.
+//  Copyright © 2016年 YehYungCheng. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
